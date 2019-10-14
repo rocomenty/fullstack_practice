161b4f8e49e972b23a05cc965384bfb7a9f783d2
rocomenty <cheng.luo@wustl.edu>
2019-10-14 16:37:44 -0700

    changed build configuration on heroku

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ app.use(passport.session());
 require('./routes/authRoute.js')(app);
 require('./routes/billingRoute.js')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // Make sure express will serve production assets from React
+    // such as main.js or main.css
+    app.use(express.static('client/build'));
+
+    // Express will serve the index.html file if it does not
+    // recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
