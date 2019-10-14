d0ed1b904237823c518c9c8a3fccd9653279b556
rocomenty <cheng.luo@wustl.edu>
2019-10-14 16:43:35 -0700

    changed routing for landing page

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,6 @@
 const passport = require('passport');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send('hello');
-    });
 
     app.get('/auth/google',
         passport.authenticate('google', {
